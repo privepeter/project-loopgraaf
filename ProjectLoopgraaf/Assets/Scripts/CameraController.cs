5df9165d3f3251933d939c865e63b25a6c7077cd
privepeter <peter_hijma@hotmail.com>
2017-09-30 17:49:37 +0200

    Better pathfinding + Camera movement

---
+++ ProjectLoopgraaf/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 origin;
+    private Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+    void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            plane.Raycast(ray, out distance);
+            origin = ray.GetPoint(distance);
+        } else if (Input.GetMouseButton(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            plane.Raycast(ray, out distance);
+            Vector3 newPosition = ray.GetPoint(distance);
+            transform.position -= (newPosition - origin);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+            Camera.main.transform.Translate(Vector3.back);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            Camera.main.transform.Translate(Vector3.forward);
+        }
+    }
+}
+
---
+++ ProjectLoopgraaf/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Player : MonoBehaviour {
+
+    NavMeshAgent pathfinder;
+    Transform target;
+    Vector3 targetPos;
+
+	void Start () {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = transform;
+        targetPos = new Vector3(target.position.x +10, 0, target.position.z + 5);
+        pathfinder.SetDestination(targetPos);
+        pathfinder.acceleration = 60f;
+    }
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            groundPlane.Raycast(ray, out distance);
+            pathfinder.SetDestination(ray.GetPoint(distance));
+        }
+    }
+}
